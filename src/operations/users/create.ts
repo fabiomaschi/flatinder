1611c4a07762c55b7d07f67c53b88e2a3418a1f2
Šimon Hrabec <simon.hrabec@gmail.com>
2021-09-25 18:28:22 +0200

    create user operations

--- src/operations/users/create.ts
+++ src/operations/users/create.ts
@@ -1,8 +1,8 @@
-// import { getCustomRepository } from 'typeorm'
+import { getCustomRepository } from 'typeorm'
 import { User, UserRole } from '../../database/entities/user'
 import { ApplicantProfile } from '../../database/entities/applicant-profile'
 import { FlatProfile } from '../../database/entities/flat-profile'
-// import { UserRepository } from '../../database/repositories/user'
+import { UserRepository } from '../../database/repositories/user'
 
 export interface CreateUserInput {
   email: string,
@@ -14,8 +14,10 @@ export interface CreateUserInput {
 }
 
 export async function CreateUser (input: CreateUserInput): Promise<User> {
-  // const repo = getCustomRepository(UserRepository)
-  const user: User = new User()
-  console.log(input)
+  const repo = getCustomRepository(UserRepository)
+  const user = await repo.create({
+    ...input,
+  })
+
   return user
 }
