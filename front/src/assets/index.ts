0cd589c4a96f300886b30d35eb1d7b97a61a9881
Tudor <tn@trainaway.fit>
2021-09-25 15:13:29 +0200

    #7 add dummy cover and profile image to application profile

---
+++ front/src/assets/index.ts
@@ -0,0 +1,4 @@
+import CoverImg from "./cover-bg.jpg";
+import DemoUser from './demo-user.jpg';
+
+export {CoverImg, DemoUser}
--- front/src/pages/ApplicantProfilePage.tsx
+++ front/src/pages/ApplicantProfilePage.tsx
@@ -1,7 +1,40 @@
-import { Header } from "semantic-ui-react";
+import { Container, Header, Image } from "semantic-ui-react";
+import { CoverImg, DemoUser } from "../assets";
+
+const style = {
+  coverContainer: {
+    height: "300px",
+    background: `url(${CoverImg}) no-repeat center`,
+    backgroundSize: "cover",
+  },
+  profileImageContainer: {
+    marginTop: "-100px",
+    height: "200px",
+    width: "200px",
+    margin: "0 auto",
+  },
+  profileImage: {
+    height: "100%",
+    width: "100%",
+    objectFit: "cover",
+  },
+};
 
 const ApplicantProfilePage = () => {
-  return <Header as="h1">User Name</Header>;
+  return (
+    <Container fluid>
+      <div style={style.coverContainer} />
+      <div style={style.profileImageContainer}>
+        <Image
+          src={DemoUser}
+          size={"medium"}
+          circular
+          style={style.profileImage}
+        />
+      </div>
+      <Header as="h1">User Name</Header>
+    </Container>
+  );
 };
 
 export default ApplicantProfilePage;
