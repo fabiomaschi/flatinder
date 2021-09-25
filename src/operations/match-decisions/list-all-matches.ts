31b16b253e1e7a02cc58517cf89a1dc8844385fc
Fabio <fabio.maschi@inf.ethz.ch>
2021-09-25 21:34:09 +0200

    compiling

--- src/operations/match-decisions/list-all-matches.ts
+++ src/operations/match-decisions/list-all-matches.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm'
 import { MatchDecision } from '../../database/entities/match-decision'
 import { MatchDecisionRepository } from '../../database/repositories/match-decision'
 import { GetUser } from '../../operations/users/get'
+import { UserRole } from '../../database/entities/user'
 
 export interface ListAllMatchesInput {
   email: string
@@ -14,14 +15,17 @@ export interface ListAllMatchesOutput {
 export async function ListAllMatches (input: ListAllMatchesInput): Promise<ListAllMatchesOutput> {
   const user = await GetUser({ email: input.email })
 
-  switch
-  const matches = getCustomRepository(MatchDecisionRepository).find( { where: {
+  let where:any  = { }
+  switch (user.role) {
+    case UserRole.APPLICANT:
+      where.applicantProfile = user.applicantProfile
+      break
+    case UserRole.FLAT:
+      where.flatProfile = user.flatProfile
+      break
+  }
+
+  const matches = await getCustomRepository(MatchDecisionRepository).find( { where })
   
-  const applicants = await repo.find({
-    where: {
-      role: UserRole.APPLICANT,
-    },
-    relations: ["applicantProfile"],
-  })
-  console.log(applicants)
+  return { matches }
 } 
